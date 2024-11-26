0b6b5a431064d6f4dd95b985588f6ece9fc74250
FaithChepkemoi <faithhope017@gmail.com>
2024-11-27 01:34:07 +0300

    my string lab updates

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'FAITHHOPE NYOLEI';
+//const welcomeMessage = 'Welcome to Flatbook, '; 
+//const welcomeMessage = 'Welcome to Flatbook, '  +  currentUser ;
+//const welcomeMessage = 'Welcome to Flatbook,  ' + currentUser;
+const welcomeMessage= `Welcome to Flatbook, ${currentUser}!`;
+//const excitedWelcomeMessage= "WELCOME TO FLATBOOK, "
+//excitedWelcomeMessage
+const excitedWelcomeMessage= `WELCOME TO FLATBOOK, ${currentUser}!`;
+const firstInitial = currentUser.charAt(0);
+const shortGreeting= `Welcome, ${firstInitial}!`;
